f09dc6f1576d347d8411b8213ac57c33eac3487a
MattGaarder <matteus.gaarder@gmail.com>
2023-02-02 16:37:40 +0000

    copies stuff from moovie to this repo

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,7 @@
 </form> */
 var OMDBKey = "407da853";
 var counter = 0;
-// var youtubeApiKey = "AIzaSyArZC4w_ZrUpGsejJW0TBItsl8bm2JQMT";
-// var videoIdo;
+var videoIdo;
 
 function OMDBInfoRequest() {
 
@@ -23,19 +22,24 @@ function OMDBInfoRequest() {
 };
 
 $("#find-movie").on("click", function(event) {
+    
     event.preventDefault();
     OMDBInfoRequest();
-    // getTrailer();
-   });
+    getTrailer()
+});
 
-   
+var discoverDiv;
+var detailsDiv;
+var playerDiv;
 function displayInfo(arbitrary) {
     // console.log("hey");
-    var discoverDiv = $("<div class='discover'>");
+    playerDiv = $("<div id='player'>")
+    discoverDiv = $("<div class='discover'>");
     var imageURL = arbitrary.Poster;
     var moviePoster = $("<img>").attr("src", imageURL);
     discoverDiv.append(moviePoster);
     var detailsDiv = $("<div class='details'>");
+    
     const keys = Object.keys(arbitrary);
     for (const key of keys) {
         if (counter === 13) break;
@@ -43,53 +47,56 @@ function displayInfo(arbitrary) {
         detailsDiv.append(theDeets);
         // console.log(arbitrary[key]);
         counter++;
+        detailsDiv.append(playerDiv);
         discoverDiv.append(detailsDiv);
     }
     $("#movies-view").append(discoverDiv);
+    getTrailer();
 }
-    // function getTrailer() {
-    //     var queryParam = $("#find-input").val();
-    //     var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + queryParam + " trailer&key=" + youtubeApiKey;
-    //     console.log(queryParam)
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //       }).then(function(youtubeObject) {
-    //         videoIdo = youtubeObject.items[0].id.videoId;
-    //         // console.log(videoIdo);
-    //         // console.log(youtubeObject);
-    //         console.log("this works");
-    //         onYouTubeIframeAPIReady(youtubeObject)
-    //       })
-    // };
+function getTrailer() {
+    var queryParam = $("#find-input").val();
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + queryParam + " trailer&key=" + youtubeApiKey;
+    console.log(queryParam)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(youtubeObject) {
+        videoIdo = youtubeObject.items[0].id.videoId;
+        // console.log(videoIdo);
+        // console.log(youtubeObject);
+        console.log("this works");
+        onYouTubeIframeAPIReady(youtubeObject)
+        })    
+};
     
     
     
-    // var tag = document.createElement('script');
-    // tag.src = "https://www.youtube.com/iframe_api";
-    // var firstScriptTag = document.getElementsByTagName('script')[0];
-    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// // 3. This function creates an <iframe> (and YouTube player)
-// //    after the API code downloads.
-// var player;
-// function onYouTubeIframeAPIReady(youtubeObject) {
-//     console.log(youtubeObject);
-//     videoIdo = youtubeObject.items[0].id.videoId;
-// player = new YT.Player('player', {
-//     height: '390',
-//     width: '640',
-//     videoId: "cPAbx5kgCJo",
-//     // switch back to videoIdo to get working again
-//     playerVars: {
-//     'playsinline': 1
-//     },
-//     // events: {
-//     // 'onReady': onPlayerReady,
-//     // 'onStateChange': onPlayerStateChange
-//     // }
-// });
-// }
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady(youtubeObject) {
+    console.log(youtubeObject);
+    videoIdo = youtubeObject.items[0].id.videoId;
+    player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: videoIdo,
+    // switch back to videoIdo to get working again
+    playerVars: {
+    'playsinline': 1
+    },
+    
+    // events: {
+    // 'onReady': onPlayerReady,
+    // 'onStateChange': onPlayerStateChange
+    // }
+});
+}
 
 // // 4. The API will call this function when the video player is ready.
 // function onPlayerReady(event) {

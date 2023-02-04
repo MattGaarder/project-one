8e68fdba14c1fac98d7b15c2a0ee25043a53177b
MattGaarder <matteus.gaarder@gmail.com>
2023-02-04 09:07:34 +0000

    stuff

--- README.md
+++ README.md
@@ -11,3 +11,57 @@
 (if I can do this before and during class, I'm happy)
 
 I want to use the API to retrieve the trailers and display them in a visually appealing way, such as in a grid or a carousel.
+
+# Separate API Keys per user in your project.
+1. Create a file at the root of your repository called .gitignore
+2. Add the following line to the file
+  keys.js
+3. Add, commit, and push your .gitignore file to the repository.
+4. Have everyone pull from the repository.
+5. Everyone should confirm they have the .gitignore file locally.
+6. Add a file in your project called keys.js
+* Because this file is in your .gitignore, git will not track it.
+7. In keys.js, define a global variable or set of variables in this file:
+
+        var omdbApiKey=3k45j4k623j6k14;
+        var openWeatherApiKey=9E92A9EV239V29V82V2;
+
+8. Use those variables in your project.
+
+        <script src="keys.js"></script>
+        <script src="project.js"></script>
+
+9. When you deploy to github pages, you will need to change the above so that it uses a file that exists. You can add a file called `public-keys.js` to your repository, and put keys in there. Then, change keys.js to  public-keys.js (for example:)
+
+        <script src="public-keys.js"></script>
+        <script src="project.js"></script>
+
+
+1. Clicking discover button calls OMDB function. DONE
+
+2. OMDB function returns object, info from which should be displayed on the page (title, year, plot, poster) in generated div. DONE
+
+3. Discover also calls youtube function. DONE
+
+4. This function returns youtube object- including videoID DONE
+
+5. Video ID from this function is stored in a variable that is inserted into iFrame video embed DONE
+(lets start off with this a MVP). I want to use the API to retrieve the trailers and display them in a visually appealing way, such as in a grid or a carousel.
+
+6. Make all this information appear presentable on the page. NOT YET. Delegate.
+
+7. Applies object keys as classes to generated p elements to apply styles in CSS. DONE 
+
+8. Add a "to watchlist" button in div to get information saved to local storage, and to add to a separate watchlist. DONE
+
+9. Add a "seen" button that adds the film to a "seen" list (again saving to local storage). DONE
+
+10. Make it so that multiple films can be displayed when clicking search button- prepended to previous film. DONE
+
+11. Create a "get a suggestion" button that will give the user a random film they can either add or move on from.
+
+12. Create a rotten tomatoes section that shows whether the film is fresh or rotten.
+
+13. Create a clear list function so that when movies are added to either list there are no repeat values. DONE
+
+14. CSS styles on watch and seen list
--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,75 @@
-// HTML
-/* <form id="movie-form">
-      <label for="movie-input">Add a Movie!</label>
-      <input type="text" id="movie-input"><br>
-      <input id="add-movie" type="submit" value="Add a Movie!">
-</form> */
-var OMDBKey = "407da853";
-var counter = 0;
+
 var videoIdo;
+var seenList = $("#seenList");
+var watchList = $("#watchList");
 
-function OMDBInfoRequest() {
-
-    var queryParam = $("#find-input").val();
-    var queryURL = "https://www.omdbapi.com/?t=" + queryParam + "&apikey=" + OMDBKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(OMDBObject) {
-        // console.log(OMDBObject);
-        displayInfo(OMDBObject);
-      })
-};
-
-$("#find-movie").on("click", function(event) {
-    
+$("#find-movie").on("click", function (event) {
+    //moving all element generation inside find-movie button
+    playerDiv = $("<div id='player'>");
+    buttonDiv = $("<div id='buttonDiv'>");
+    discoverDiv = $("<div class='discover'>");
+    seenButton = $("<button id='seen'>");
+    watchButton = $("<button id='watch'>");
+    detailsDiv = $("<div class='details'>");
+    $("#movies-view").append(discoverDiv);
     event.preventDefault();
     OMDBInfoRequest();
     getTrailer()
 });
 
-var discoverDiv;
-var detailsDiv;
-var playerDiv;
-function displayInfo(arbitrary) {
-    // console.log("hey");
-    playerDiv = $("<div id='player'>")
-    discoverDiv = $("<div class='discover'>");
-    var imageURL = arbitrary.Poster;
+function OMDBInfoRequest() {
+    var queryParam = $("#find-input").val();
+    var queryURL = "https://www.omdbapi.com/?t=" + queryParam + "&apikey=" + OMDBKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (OMDBObject) {
+        displayInfo(OMDBObject);
+    })
+};
+
+
+function displayInfo(OMDBCall) {
+    var imageURL = OMDBCall.Poster;
     var moviePoster = $("<img>").attr("src", imageURL);
     discoverDiv.append(moviePoster);
-    var detailsDiv = $("<div class='details'>");
-    
-    const keys = Object.keys(arbitrary);
+    const keys = Object.keys(OMDBCall);
+    let i = 0;
     for (const key of keys) {
-        if (counter === 13) break;
-        var theDeets = $("<p>").text(arbitrary[key])
+        if (i === 13) break;
+        if (i === 3 || i === 7 || i === 11) {
+            i++;
+            continue;
+        }
+        var theDeets = $("<p>").text(OMDBCall[key]).addClass(key);
         detailsDiv.append(theDeets);
-        // console.log(arbitrary[key]);
-        counter++;
-        detailsDiv.append(playerDiv);
-        discoverDiv.append(detailsDiv);
+        i++;
     }
-    $("#movies-view").append(discoverDiv);
+    movieObject = {
+        Title: OMDBCall.Title,
+        Year: OMDBCall.Year,
+        Poster: OMDBCall.Poster,
+    };
+    console.log(movieObject);
+    console.log(OMDBCall.Title);
+    appendElements(movieObject);
     getTrailer();
+};
+
+function appendElements(OMDBCall) {
+    discoverDiv.append(detailsDiv);
+    discoverDiv.append(playerDiv);
+    detailsDiv.append(buttonDiv);
+    buttonDiv.append(watchButton);
+    buttonDiv.append(seenButton);
+    seenButton.on('click', function () {
+        moveToSeen(OMDBCall);
+    });
+    watchButton.on('click', function () {
+        moveToWatch(OMDBCall);
+    });
 }
+
 function getTrailer() {
     var queryParam = $("#find-input").val();
     var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + queryParam + " trailer&key=" + youtubeApiKey;
@@ -60,43 +77,92 @@ function getTrailer() {
     $.ajax({
         url: queryURL,
         method: "GET"
-        }).then(function(youtubeObject) {
+    }).then(function (youtubeObject) {
         videoIdo = youtubeObject.items[0].id.videoId;
-        // console.log(videoIdo);
-        // console.log(youtubeObject);
-        console.log("this works");
         onYouTubeIframeAPIReady(youtubeObject)
-        })    
+    })
 };
-    
-    
-    
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady(youtubeObject) {
-    console.log(youtubeObject);
     videoIdo = youtubeObject.items[0].id.videoId;
     player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: videoIdo,
-    // switch back to videoIdo to get working again
-    playerVars: {
-    'playsinline': 1
-    },
-    
-    // events: {
-    // 'onReady': onPlayerReady,
-    // 'onStateChange': onPlayerStateChange
-    // }
-});
+        height: '390',
+        width: '640',
+        videoId: videoIdo,
+        playerVars: {
+            'playsinline': 1
+        },
+    });
 }
+function moveToSeen(movieObject) {
+    console.log(movieObject);
+    var seenArray = JSON.parse(localStorage.getItem("seenArray")) || [];
+    for(var i = 0; i < seenArray.length; i++) {
+        if(movieObject.Title === seenArray[i].Title) {
+            return;
+        }
+    }
+        clearArray(seenList);
+        seenArray.push(movieObject);
+        localStorage.setItem("seenArray", JSON.stringify(seenArray));
+        createSeenArray();
+}
+
+function createSeenArray() {
+    var seenArray = JSON.parse(localStorage.getItem("seenArray")) || [];
+    for(var i = 0; i < seenArray.length; i++) {
+        var seenItem = $("<li>");
+        seenItem.text(seenArray[i].Title);
+        seenList.append(seenItem);
+    }
+};
+createSeenArray();
+
+function moveToWatch(movieObject) {
+    console.log(movieObject);
+    var watchArray = JSON.parse(localStorage.getItem("watchArray")) || [];
+    for(var i = 0; i < watchArray.length; i++) {
+        if(movieObject.Title === watchArray[i].Title) {
+            return;
+        }
+    }   
+        clearArray(watchList)
+        watchArray.push(movieObject);
+        localStorage.setItem("watchArray", JSON.stringify(watchArray));
+        createWatchArray();
+}
+
+function createWatchArray() {
+    var watchArray = JSON.parse(localStorage.getItem("watchArray")) || [];
+    for(var i = 0; i < watchArray.length; i++) {
+        var watchItem = $("<li>");
+        watchItem.text(watchArray[i].Title);
+        watchList.append(watchItem);
+    }
+};
+createWatchArray();
+
+function clearArray(array) {
+    array.empty();
+}
+
+
+// events: {
+// 'onReady': onPlayerReady,
+// 'onStateChange': onPlayerStateChange
+// }
+
+
+
+
 
 // // 4. The API will call this function when the video player is ready.
 // function onPlayerReady(event) {
